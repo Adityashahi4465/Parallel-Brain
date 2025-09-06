7091f9cb0e67e0c7a46ab100158bfc99a16ca371
20AbhishekRoka02 <75659036+20AbhishekRoka02@users.noreply.github.com>
2025-09-06 23:20:12 +0530

    uncommented, ollama install and curl install lines

--- backend-llm/app/DeadStockAI.py
+++ backend-llm/app/DeadStockAI.py
@@ -16,8 +16,8 @@ def connectingWithOllama():
 			sleep(1) # Sleep for 3 seconds as server may take around this much time.
    
 def OllamaSetup():
-	# os.system("apt update && apt upgrade -y && apt install curl -y")
-	# os.system("curl -sS https://ollama.ai/install.sh | bash")
+	os.system("apt update && apt upgrade -y && apt install curl -y")
+	os.system("curl -sS https://ollama.ai/install.sh | bash")
 	os.system("nohup ollama serve &")
 	connectingWithOllama()
 	os.system("ollama pull phi3")
